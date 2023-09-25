ed5641d5f445eab1ef43a2ecc2b6efd29fda2f3e
VW <vera.weikel@gmail.com>
2023-09-25 14:20:29 -0700

    Create sever.js and add type Task.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const mongoose = require('mongoose');
+
+// define gql schema
+const typeDefs = gql`
+    type Task {
+        id: ID!
+        title: String!
+        completed: Boolean!
+    }
+        
+`
